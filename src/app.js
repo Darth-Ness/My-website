e202fc787d828fa28b8723c4b0e64d063fe6cf2d
Darth-Ness <75047141+Darth-Ness@users.noreply.github.com>
2025-01-29 03:16:08 +0000

    Fix banner issue

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-fetch("banner.html")
+fetch("https://darth-ness.github.io/my-website/banner.html")
     .then((response) => response.text())
     .then((data) => document.getElementById("banner").innerHTML = data);
 
