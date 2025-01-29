4c9677d7b5d9d1ac380564e9c9c1c4b45aabb7f6
Sky <reeuuthdc80j@opayq.net>
2025-01-28 21:08:00 -0500

    Move banner code outside of js

--- src/app.js
+++ src/app.js
@@ -1,8 +1,4 @@
-function component(element, code) {document.querySelector(element).insertAdjacentHTML("afterend", code);}
-component('#banner', `<div class="header p8"> \
-             <a href="index.html"><h1 class="red sameline">Darth-Ness' Website</h1></a> \
-             <a href="sites.html"><button class="right">Sites</button></a> \
-             <a href="photos.html"><button class="right">Photos</button></a> \
-             <a href="coding.html"><button class="right">Coding</button></a> \
-             <a href="blog.html"><button class="right">Blog</button></a> \
-         </div>`);
+fetch("banner.html")
+    .then((response) => response.text())
+    .then((data) => document.getElementById("banner").innerHTML = data);
+
